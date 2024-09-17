5b67d454a61c9877ccf9239300b9ee5766793273
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-09-17 20:52:49 -0300

    refactor: remove old migrations

--- src/drizzle/migrations/0000_loud_bushwacker.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS "users" (
-	"id" varchar(14) PRIMARY KEY NOT NULL,
-	"name" text NOT NULL,
-	"email" text NOT NULL,
-	"created_at" timestamp DEFAULT now() NOT NULL,
-	"updated_at" timestamp DEFAULT now() NOT NULL,
-	CONSTRAINT "users_email_unique" UNIQUE("email")
-);
