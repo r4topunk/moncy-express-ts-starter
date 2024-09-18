103dc421ccc19dca5d4e21b40d6860a3d01b5601
arthursiq5 <arthur.siq5@gmail.com>
2024-09-18 16:22:52 -0300

    Added JWT to routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,6 +5,8 @@ import httpStatus from 'http-status';
 
 import { db } from '#src/drizzle/index.ts';
 import { redirects } from '#src/drizzle/schema/redirects.ts';
+import { decodeJWT, encodeJWT } from '#src/utils/JWTRoutes.ts';
+import { JwtPayload } from 'jsonwebtoken';
 
 const router = express.Router();
 
@@ -20,9 +22,14 @@ router.get('/routes', async (req, res) => {
       .select()
       .from(redirects)
 
-    res.status(httpStatus.OK).json(result)
+    const ans = result.map(row => ({
+      ...row,
+      link: `http://localhost:9500/redirect/jwt/${encodeJWT({ uuid: row.uuid })}`
+    }))
+
+    res.status(httpStatus.OK).json(ans)
   } catch (error) {
-    
+
   }
 })
 
@@ -34,6 +41,33 @@ router.get('/redirect/:uuid', async (req, res) => {
       .select()
       .from(redirects)
       .where(eq(redirects.uuid, uuid));
+
+    if (result.length === 0) {
+      return res.status(httpStatus.NOT_FOUND).json({
+        message: 'Redirect not found',
+      });
+    }
+
+    return res.redirect(result[0].url);
+  } catch (error) {
+    console.error(error);
+    return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+      message: 'An error occurred',
+    });
+  }
+});
+
+router.get('/redirect/jwt/:jwt', async (req, res) => {
+  interface CustomRequest extends Request {
+    token: string | JwtPayload;
+  }
+  try {
+    const { uuid } = decodeJWT(req.params.jwt)
+
+    const result = await db
+      .select()
+      .from(redirects)
+      .where(eq(redirects.uuid, uuid as string));
 
     if (result.length === 0) {
       return res.status(httpStatus.NOT_FOUND).json({
